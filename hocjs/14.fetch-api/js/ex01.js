2b217151cb9fa0ef67b9d1cb2a164d657e44c3d7
Hoang An Unicode <hoangan.web@gmail.com>
2026-01-10 22:21:05 +0700

    Update code

--- hocjs/14.fetch-api/js/ex01.js
+++ hocjs/14.fetch-api/js/ex01.js
@@ -93,12 +93,13 @@ const app = {
               <button
                 class="border rounded-full px-4 py-1 hover:bg-gray-100 transition"
                 data-id="${value.id}"
+                data-type="detail"
               >
                 Xem chi tiết
               </button>
               <div class="space-x-2">
-                <button class="text-gray-600 hover:underline">Sửa</button>
-                <button class="text-red-500 hover:underline">Xóa</button>
+                <button class="text-gray-600 hover:underline" data-id="${value.id}" data-type="edit">Sửa</button>
+                <button class="text-red-500 hover:underline" data-id="${value.id}" data-type="delete">Xóa</button>
               </div>
             </div>
           </div>`
@@ -112,23 +113,152 @@ const app = {
     const overlayEl = modalEl.querySelector(".js-overlay");
     const resetModal = () => {
       const h2 = modalEl.querySelector("h2");
-      const p = modalEl.querySelector("p");
+      const modalContent = modalEl.querySelector(".js-modal-content");
       h2.innerText = "";
-      p.innerText = "";
+      modalContent.innerText = "";
     };
     btnList.forEach((btn) => {
       btn.addEventListener("click", async () => {
-        modalEl.classList.remove("hidden");
         const id = btn.dataset.id;
-        const h2 = modalEl.querySelector("h2");
-        const p = modalEl.querySelector("p");
-        h2.innerText = "Loading title...";
-        p.innerText = "Loading content...";
-        const post = await this.getPost(id);
-        h2.innerText = post.title;
-        p.innerText = post.content;
+        if (btn.dataset.type === "detail") {
+          modalEl.classList.remove("hidden");
+          const h2 = modalEl.querySelector("h2");
+          const modalContent = modalEl.querySelector(".js-modal-content");
+
+          h2.innerText = "Loading title...";
+          modalContent.innerText = "Loading content...";
+
+          const post = await this.getPost(id);
+
+          h2.innerText = post.title;
+          modalContent.innerText = post.content;
+        }
+
+        if (btn.dataset.type === "edit") {
+          modalEl.classList.remove("hidden");
+          const h2 = modalEl.querySelector("h2");
+          const modalContent = modalEl.querySelector(".js-modal-content");
+          h2.innerText = "Cập nhật bài viết";
+          modalContent.innerHTML = `
+          <form>
+            <div class="mb-3">
+              <label class="block mb-1 font-semibold">Tiêu đề</label>
+              <input type="text" placeholder="Tiêu đề..." class="border js-title border-gray-300 px-3 py-2 w-full outline-none"/> 
+            </div>
+            <div class="mb-3">
+              <label class="block mb-1 font-semibold">Nội dung</label>
+              <textarea placeholder="Tiêu đề..." class="js-content border border-gray-300 px-3 py-2 w-full outline-none min-h-[100px]"></textarea> 
+            </div>
+            <button class="bg-green-600 text-white px-5 py-2 rounded-full cursor-pointer">Lưu lại</button>
+          </form>
+          `;
+          const titleEl = modalContent.querySelector(".js-title");
+          const contentEl = modalContent.querySelector(".js-content");
+          const post = await this.getPost(id);
+          titleEl.value = post.title;
+          contentEl.value = post.content;
+
+          //Lắng nghe form
+          const formEl = modalEl.querySelector("form");
+          formEl.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            const titleEl = formEl.querySelector(".js-title");
+            const contentEl = formEl.querySelector(".js-content");
+            const title = titleEl.value;
+            const content = contentEl.value;
+
+            if (!title || !content) {
+              return alert("Vui lòng nhập dữ liệu");
+            }
+            const updateStatus = await this.updatePost({ title, content }, id);
+            if (!updateStatus) {
+              return alert("Cập nhật không thành công");
+            }
+            modalEl.classList.add("hidden");
+            resetModal();
+            this.getPosts();
+          });
+        }
+
+        if (btn.dataset.type === "delete") {
+          modalEl.classList.remove("hidden");
+          const h2 = modalEl.querySelector("h2");
+          const modalContent = modalEl.querySelector(".js-modal-content");
+          h2.innerText = "Bạn có chắc chắn?";
+          modalContent.innerHTML = `
+          <p>Nếu bấm đồng ý, bạn sẽ không khôi phục lại được</p>
+           <div class="mt-3 flex justify-end gap-3">
+           <button class="js-confirm bg-green-600 text-white px-5 py-2 rounded-full cursor-pointer">Đồng ý</button>
+           <button class="js-cancel bg-red-600 text-white px-5 py-2 rounded-full cursor-pointer">Hủy</button>
+           </div>
+          `;
+
+          const cancelBtn = modalContent.querySelector(".js-cancel");
+          const confirmBtn = modalContent.querySelector(".js-confirm");
+          cancelBtn.addEventListener("click", () => {
+            modalEl.classList.add("hidden");
+            resetModal();
+          });
+          confirmBtn.addEventListener("click", async () => {
+            const deleted = await this.deletePost(id);
+            if (!deleted) {
+              return alert("Không thể xóa");
+            }
+            modalEl.classList.add("hidden");
+            resetModal();
+            this.getPosts();
+          });
+        }
       });
     });
+    //Xử lý khi bấm nút thêm
+
+    const addBtn = document.querySelector(".js-add-btn");
+    addBtn.addEventListener("click", () => {
+      modalEl.classList.remove("hidden");
+      const h2 = modalEl.querySelector("h2");
+      const modalContent = modalEl.querySelector(".js-modal-content");
+      h2.innerText = `Thêm bài viết`;
+      modalContent.innerHTML = `
+      <form>
+        <div class="mb-3">
+          <label class="block mb-1 font-semibold">Tiêu đề</label>
+          <input type="text" placeholder="Tiêu đề..." class="border js-title border-gray-300 px-3 py-2 w-full outline-none"/> 
+        </div>
+        <div class="mb-3">
+          <label class="block mb-1 font-semibold">Nội dung</label>
+          <textarea placeholder="Tiêu đề..." class="js-content border border-gray-300 px-3 py-2 w-full outline-none min-h-[100px]"></textarea> 
+        </div>
+        <button class="bg-green-600 text-white px-5 py-2 rounded-full cursor-pointer">Lưu lại</button>
+      </form>
+      `;
+
+      //Lắng nghe form
+      const formEl = modalEl.querySelector("form");
+      formEl.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const titleEl = formEl.querySelector(".js-title");
+        const contentEl = formEl.querySelector(".js-content");
+        const title = titleEl.value;
+        const content = contentEl.value;
+
+        if (!title || !content) {
+          return alert("Vui lòng nhập dữ liệu");
+        }
+        const addPostStatus = await this.addPost({ title, content });
+        if (addPostStatus) {
+          //Đóng modal
+          modalEl.classList.add("hidden");
+          resetModal();
+
+          //Gọi lại dữ liệu
+          this.getPosts();
+        } else {
+          alert("Đã có lỗi xảy ra khi thêm");
+        }
+      });
+    });
+
     closeBtnEl.addEventListener("click", () => {
       modalEl.classList.add("hidden");
       resetModal();
@@ -160,11 +290,11 @@ const app = {
     const response = await fetch(`${BASE_URL}/posts/${id}`);
     return response.json();
   },
-  async getPosts() {
+  async getPosts(keyword = "") {
     //Call api
     try {
       this.renderLoading(true);
-      const response = await fetch(`${BASE_URL}/posts`);
+      const response = await fetch(`${BASE_URL}/posts?q=${keyword}`);
       const data = await response.json();
       this.render(data);
     } catch (error) {
@@ -173,8 +303,69 @@ const app = {
       this.renderLoading(false);
     }
   },
+  async addPost(data) {
+    try {
+      const response = await fetch(`${BASE_URL}/posts`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error("Add post failed");
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  async updatePost(data, id) {
+    try {
+      const response = await fetch(`${BASE_URL}/posts/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error("Update post failed");
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  async deletePost(id) {
+    try {
+      const response = await fetch(`${BASE_URL}/posts/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Delete post failed");
+      }
+      return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  searchPosts() {
+    const inputEl = document.querySelector(".js-search");
+    let id;
+    inputEl.addEventListener("input", (e) => {
+      if (id) {
+        clearTimeout(id);
+      }
+      id = setTimeout(() => {
+        const q = e.target.value;
+        this.getPosts(q);
+      }, 500);
+    });
+  },
   start() {
     this.getPosts();
+    this.searchPosts();
   },
 };
 
